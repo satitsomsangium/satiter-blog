2cd52b3db529a137fda63ef65351cf961714b81a
satitbeid <satit@beid.io>
2026-04-14 20:10:14 +0700

    implement social link

--- lib/social.ts
+++ lib/social.ts
@@ -12,12 +12,12 @@ export type SocialLink = {
 };
 
 const allSocialLinks: SocialLink[] = [
-  { network: "facebook", href: "https://www.facebook.com/", label: "Facebook" },
-  { network: "x", href: "https://twitter.com/", label: "X (Twitter)" },
-  { network: "github", href: "https://github.com/", label: "GitHub" },
-  { network: "youtube", href: "https://www.youtube.com/", label: "YouTube" },
-  { network: "instagram", href: "https://www.instagram.com/", label: "Instagram" },
-  { network: "rss", href: "https://www.satiter.com/feeds/posts/default", label: "RSS" },
+  { network: "facebook", href: "https://www.facebook.com/satitsomsangium", label: "Facebook" },
+  { network: "x", href: "https://x.com/SatitSomsangium", label: "X (Twitter)" },
+  // { network: "github", href: "https://github.com/", label: "GitHub" },
+  // { network: "youtube", href: "https://www.youtube.com/", label: "YouTube" },
+  { network: "instagram", href: "https://www.instagram.com/satitsomsangium/", label: "Instagram" },
+  // { network: "rss", href: "https://www.satiter.com/feeds/posts/default", label: "RSS" },
 ];
 
 export const socialLinks: SocialLink[] = allSocialLinks.filter((item) => item.href.trim().length > 0);
